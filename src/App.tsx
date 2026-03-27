c4f24b2118ae9121c024777e12cae294f2b1aa8f
Aaryan <itztogi@gmail.com>
2026-03-27 22:07:06 +0530

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import Game from "./Game";
 
 const App: React.FC = () => {
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/game" element={<Game />} />
             </Routes>
-        </BrowserRouter>
+        </Router>
     );
 };
 
